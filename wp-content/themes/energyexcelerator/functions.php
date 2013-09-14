18df2bc7aa153dcfa32ccbb6098b94266a0b49ee
bryanmonzon <monzonb@gmail.com>
2013-09-13 17:10:03 -0700

    added more plugins and updated some template files

---
+++ wp-content/themes/energyexcelerator/functions.php
@@ -0,0 +1,13 @@
+<?php
+
+// This little bit of code allows you to rename the options title and subpages, if needed.
+/* 
+acf_set_options_page_title( __('Theme Options') );
+
+if( function_exists('acf_add_options_sub_page') )
+{
+    acf_add_options_sub_page(array(
+        'title' => 'Footer',
+        'capability' => 'manage_options'
+    ));
+} */
