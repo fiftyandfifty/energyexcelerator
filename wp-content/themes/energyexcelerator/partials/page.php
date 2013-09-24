bb20be0011045e0b4c2ac1f69145c2f67d972028
bryanmonzon <monzonb@gmail.com>
2013-09-24 13:53:42 -0700

    adding container widths to child theme

--- wp-content/themes/energyexcelerator/partials/page.php
+++ wp-content/themes/energyexcelerator/partials/page.php
@@ -1,5 +1,8 @@
+<?php // get the container class (section_width ACF)	
+		$section_width = get_sub_field('section_width');
+?>
 <section>
-	<div class="container">
+	<div class="<?php echo $section_width; ?>">
 		<h1><?php the_title(); ?></h1>
 		<?php the_content(); ?>
 	</div>
