2e242c6c43385d774a5ce488a226e3f21c94149f
bryanmonzon <monzonb@gmail.com>
2013-09-24 14:28:38 -0700

    add flex container to page

--- wp-content/themes/energyexcelerator/partials/page-external.php
+++ wp-content/themes/energyexcelerator/partials/page-external.php
@@ -1,5 +1,6 @@
 <?php $select_page = get_sub_field('select_page', 'option'); ?>
 <?php $sel_page_id = $select_page->ID; ?>
+<?php $section_width = get_sub_field('section_width'); // get the container class (section_width ACF) ?>
 
 <?php 
 $page_args = array(
@@ -9,9 +10,11 @@ $page_args = array(
 $page_query = new WP_query( $page_args);
 if( $page_query->have_posts() ) : while( $page_query->have_posts() ) : $page_query->the_post(); ?>
 
-<div class="container">
-	<h1><?php the_title(); ?></h1>
-	<?php the_excerpt(); ?>
-</div>
+<section>
+	<div class="<?php echo $section_width; ?>">
+		<h1><?php the_title(); ?></h1>
+		<?php the_excerpt(); ?>
+	</div>
+</section>
 
 <?php endwhile; endif; wp_reset_query(); ?>
