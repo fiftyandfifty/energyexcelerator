223537c22426caf468a2df975d1f460ba44f3506
bryanmonzon <monzonb@gmail.com>
2013-10-01 10:07:36 -0700

    added padding-bottom

--- wp-content/themes/energyexcelerator/partials/general-use.php
+++ wp-content/themes/energyexcelerator/partials/general-use.php
@@ -1,11 +1,12 @@
 <?php 
-	$section_content	= get_sub_field('general_content');
-	$background_color 	= get_sub_field('background_color'); 
-	$text_color 		= get_sub_field('text_color');
-	$background_image 	= get_sub_field('background_image');
-	$repeat_image		= get_sub_field('repeat_image');
-	$section_width 		= get_sub_field('section_width'); // get the container class (section_width ACF)
-	$section_padding  	= get_sub_field('section_padding');
+	$section_content		= get_sub_field('general_content');
+	$background_color 		= get_sub_field('background_color'); 
+	$text_color 			= get_sub_field('text_color');
+	$background_image 		= get_sub_field('background_image');
+	$repeat_image			= get_sub_field('repeat_image');
+	$section_width 			= get_sub_field('section_width'); // get the container class (section_width ACF)
+	$section_padding_top	= get_sub_field('section_padding_top');
+	$section_padding_bottom	= get_sub_field('section_padding_bottom');
 	
 
 	if( $background_color ){
@@ -38,8 +39,8 @@
 
 	}
 
-	if( $section_padding ){
-		$section_padding = 'padding:' . $section_padding . 'px 0; ';
+	if( $section_padding_top || $section_padding_bottom ){
+		$section_padding = 'padding:' . $section_padding_top . 'px 0 ' . $section_padding_bottom . 'px 0; ';
 	}
  ?>
 <section style="<?php echo $background_color. $text_color . $background_image . $section_padding; ?>">
