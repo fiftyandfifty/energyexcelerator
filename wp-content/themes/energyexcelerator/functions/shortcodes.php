f19c3d123e71d8cf91cef32b3128eab9df0fb548
bryanmonzon <monzonb@gmail.com>
2013-10-01 16:12:58 -0700

    ffw and shortcodes

--- wp-content/themes/energyexcelerator/functions/shortcodes.php
+++ wp-content/themes/energyexcelerator/functions/shortcodes.php
@@ -15,24 +15,6 @@ function ffw_button_shortcodes( $atts, $content=null ){
 add_shortcode( 'button', 'ffw_button_shortcodes' );
 
 
-
-//Setup Icon Shortcodes
-function ffw_icon_shortcodes( $atts ){
-	extract( shortcode_atts( array( 
-			'type'	=>	'',
-			'style'	=>	''
-		), $atts ) );
-
-	$icon_style = 'style="' . $style . '"';
-
-	$ffw_icon = '<i class="icon ' . $type . '" '. $icon_style .'></i>';
-
-	return $ffw_icon;
-	
-}
-add_shortcode( 'icon', 'ffw_icon_shortcodes' );
-
-
 function ffw_grid_shortcode( $atts, $content=null ){
 	extract( shortcode_atts( array(
 		'type'	=> ''
