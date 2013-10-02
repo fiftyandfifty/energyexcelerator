38661bc8cc563c6b9daf15b490e5c995a5f743b2
bryanmonzon <monzonb@gmail.com>
2013-10-02 15:15:42 -0700

    fix container issue

--- wp-content/themes/energyexcelerator/single-ffw_portfolio.php
+++ wp-content/themes/energyexcelerator/single-ffw_portfolio.php
@@ -7,23 +7,23 @@
 <?php do_action('FFW_hero_after'); ?>
 
 <div id="main" class="page page-default default">
-	
-	<div id="sidebar-default" class="sidebar collapsable collapsed push-<?php sidebar_position_class(); ?>">
-	  <div id="sidebar-toggle"></div>
-	  <?php get_sidebar(); ?>
-	</div><!-- #sidebar -->
+	<div class="container">
+		<div id="sidebar-default" class="sidebar collapsable collapsed push-<?php sidebar_position_class(); ?>">
+		  <div id="sidebar-toggle"></div>
+		  <?php get_sidebar(); ?>
+		</div><!-- #sidebar -->
     
-    <div id="content" class="push-<?php sidebar_position_class(); ?>">
+	    <div id="content" class="push-<?php sidebar_position_class(); ?>">
 
-      <div class="content-inner">
-        
-        <?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
-          <?php get_template_part( 'loop/loop', get_post_type() ); ?>
-        <?php endwhile; endif; ?>
+	      <div class="content-inner">
+	        
+	        <?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
+	          <?php get_template_part( 'loop/loop', get_post_type() ); ?>
+	        <?php endwhile; endif; ?>
 
-      </div><!-- .content-inner -->
-    </div><!-- #content -->
-  </div><!-- .container -->
+	      </div><!-- .content-inner -->
+	    </div><!-- #content -->
+	</div><!-- .container -->
 </div><!-- #main -->
 
 <?php get_footer(); ?>
