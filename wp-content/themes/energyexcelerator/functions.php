35c520d470f3f416d26de5741ee6b883e2847736
bryanmonzon <monzonb@gmail.com>
2013-10-03 14:45:49 -0700

    adjustment

--- wp-content/themes/energyexcelerator/functions.php
+++ wp-content/themes/energyexcelerator/functions.php
@@ -4,7 +4,6 @@ require_once( get_stylesheet_directory() .'/functions/ffw-posttype-rewrites.php'
 require_once( get_stylesheet_directory() .'/functions/images.php' );
 require_once( get_stylesheet_directory() .'/functions/acf-options.php' );
 require_once( get_stylesheet_directory() .'/functions/misc.php' );
-require_once( get_stylesheet_directory() .'/functions/sidebars.php' );
 require_once( get_stylesheet_directory() .'/functions/admin-styles.php' );
 require_once( get_stylesheet_directory() .'/functions/gravityforms.php' );
 
--- wp-content/themes/energyexcelerator/functions/sidebars.php
+++
@@ -1,4 +0,0 @@
-<?php
-/**
-* Register additional sidebars, dynamically set the names based on the post types. 
-*/
