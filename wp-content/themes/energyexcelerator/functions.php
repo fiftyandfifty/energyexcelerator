c61e22319b2f5329078482786900a911cb59ffc3
bryanmonzon <monzonb@gmail.com>
2013-10-04 09:19:19 -0700

    come on man

--- wp-content/themes/energyexcelerator/functions.php
+++ wp-content/themes/energyexcelerator/functions.php
@@ -1,5 +1,5 @@
 <?php
-/* Required files */
+/* Required files for this child theme. */
 require_once( get_stylesheet_directory() .'/functions/ffw-posttype-rewrites.php' );
 require_once( get_stylesheet_directory() .'/functions/images.php' );
 require_once( get_stylesheet_directory() .'/functions/acf-options.php' );
