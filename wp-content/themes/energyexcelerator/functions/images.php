5abf2d08be88d5220ddac591932ceeffd057d76b
bryanmonzon <monzonb@gmail.com>
2013-10-07 17:24:36 -0700

    revised ffw-grid add_image_size parameters to 600x428

--- wp-content/themes/energyexcelerator/functions/images.php
+++ wp-content/themes/energyexcelerator/functions/images.php
@@ -3,5 +3,5 @@
 if( function_exists( 'add_theme_support' ) ){
 
 	add_theme_support( 'post_thumbnails' );
-	add_image_size( 'ffw-grid', 600, 600, true );
+	add_image_size( 'ffw-grid', 600, 428, true );
 }
