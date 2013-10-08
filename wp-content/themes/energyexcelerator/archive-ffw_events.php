47e29c16a5c0f4f0eac57b79277ab83526a7d023
Alexander Zizzo <elzi@nerdi.net>
2013-10-08 14:32:51 -0700

    ffw update shapes

--- wp-content/themes/energyexcelerator/archive-ffw_events.php
+++ wp-content/themes/energyexcelerator/archive-ffw_events.php
@@ -7,14 +7,16 @@
 <div id="main" class="page page-default default">
 	<div class="container">
     <div class="row">
-      <h1 class="section-title">
-        <?php 
-          if ( function_exists( 'ffw_events_get_label_plural' ) ) : 
-            echo ffw_events_get_label_plural(); 
-          else : 
-            echo 'Events';
-          endif;  ?>
-      </h1>
+      <header class="section-header">
+      	<h1 class="section-title">
+	        <?php 
+	          if ( function_exists( 'ffw_events_get_label_plural' ) ) : 
+	            echo ffw_events_get_label_plural(); 
+	          else : 
+	            echo 'Events';
+	          endif;  ?>
+	      </h1>
+      </header>
     </div>
 
 		<div id="sidebar-default" class="sidebar collapsable collapsed push-<?php sidebar_position_class(); ?>">
@@ -26,20 +28,26 @@
       <div class="content-inner">
         
         <?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
-        	
-          <article class="post post-<?php echo get_the_ID(); ?>">
-            <header>
-              <h1 class="post-title">
-                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>  
-              </h1>
-            </header>
-            <div class="content">
-              <?php the_content(); ?>
+
+          <article class="post post-events post-<?php echo get_the_ID(); ?>">
+            <div class="post-thumb med">
+            	<div class="circle-date">
+            		<div class="circle-date-inner">
+            			<i class="icon icon-calendar"></i>
+            			<span class="date-month"><?php the_time('F'); ?></span>
+            			<b class="date-day"><?php the_time('j'); ?></b>
+            		</div>
+            	</div>
             </div>
-            <footer>
-              <?php do_action('FFW_post_details'); ?>
-            </footer>
+            <div class="post-content">
+            	<h1 class="post-title">
+            	  <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>  
+            	</h1>
+            	<?php do_action('FFW_post_details'); ?>
+            </div>
           </article>
+
+
         <?php endwhile; endif; ?>
 
       </div><!-- .content-inner -->
