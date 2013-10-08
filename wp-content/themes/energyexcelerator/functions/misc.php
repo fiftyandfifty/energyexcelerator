aabac38e4923adde1fd24c17b8d4fed0a3fe1da8
bryanmonzon <monzonb@gmail.com>
2013-10-08 11:59:38 -0700

    sttttuuuufff with ffw update too by alexander zizzzzzo

--- wp-content/themes/energyexcelerator/functions/misc.php
+++ wp-content/themes/energyexcelerator/functions/misc.php
@@ -13,3 +13,23 @@ function ee_add_excerpts_to_pages() {
 
 //Allow shortcodes to be used in excerpts
 add_filter('the_excerpt', 'do_shortcode');
+
+
+function ee_login_logo() { ?>
+    <style type="text/css">
+        body.login div#login h1 a {
+            background-image: url("<?php echo get_bloginfo( 'url' ); ?>/wp-content/uploads/2013/09/ee_logo.png");
+            padding-bottom: 15px;
+            background-size:123px 63px;
+        }
+        body.login{
+        	background:#fff;
+        }
+
+        .login form{
+        	-webkit-border-radius: 0px;
+			border-radius: 0px;
+        }
+    </style>
+<?php }
+add_action( 'login_enqueue_scripts', 'ee_login_logo' );
