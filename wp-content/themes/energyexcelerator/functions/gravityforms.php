2863cc62041f8fd9894c029979f3b16e1f750f6d
bryanmonzon <monzonb@gmail.com>
2013-10-29 11:51:16 -0700

    ffw updates and move gforms updates to FFW

--- wp-content/themes/energyexcelerator/functions/gravityforms.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// filter the Gravity Forms button type test
-add_filter("gform_submit_button", "form_submit_button", 10, 2);
-function form_submit_button($button, $form){
-
-    return "<button class='btn secondary' id='gform_submit_button_{$form["id"]}'>{$form["button"]["text"]}</button>";
-
-}
